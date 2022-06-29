1d652fd8919c8efb1b6d463089717e491348e9e2
NHower <howernickoli@gmail.com>
2022-06-28 20:37:44 -0400

    should catch non-number inputs

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,8 +1,8 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById("calc-form");
   if (form) {
     setupIntialValues();
-    form.addEventListener("submit", function(e) {
+    form.addEventListener("submit", function (e) {
       e.preventDefault();
       update();
     });
@@ -37,17 +37,22 @@ function update() {
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
-  let pir = values['rate']/1200; //periodic interest rate
-  console.log(`periodic interest rate = ${pir}`);
-  let nop = values['years']*12; //number of payments
-  console.log(`number of payments = ${nop}`);
-  let divisor = (1 - Math.pow((1 + pir),(-nop)));
-  let monpay = 0;
-  if (divisor !== 0){ //prevent divison by 0 (if rate is 0)
-    monpay = (values['amount'] * pir) / divisor;
+  if (isNaN(values['amount']) || isNaN(values['rate']) || isNaN(values['years'])) {
+    return 'Please input valid numbers'
+  } else {
+    let pir = values['rate'] / 1200; //periodic interest rate
+    console.log(`periodic interest rate = ${pir}`);
+    let nop = values['years'] * 12; //number of payments
+    console.log(`number of payments = ${nop}`);
+    let divisor = (1 - Math.pow((1 + pir), (-nop)));
+    let monpay = 0;
+    console.log(`Math is ${values['amount'] * pir} divided by ${divisor}`)
+    if (divisor !== 0) { //prevent divison by 0 (if rate is 0)
+      monpay = (values['amount'] * pir) / divisor;
+    }
+    console.log(monpay);
+    return `$ ${monpay.toFixed(2)}`;
   }
-  console.log(monpay);
-  return `$ ${monpay.toFixed(2)}`;
 }
 
 // Given a string representing the monthly payment value,
