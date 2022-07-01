34a87f89a5f130b2b082c722630e54f838102ab6
NHower <howernickoli@gmail.com>
2022-07-01 14:20:49 -0400

    updated servers.test.js with new tests

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,4 +1,4 @@
-describe("Servers test (with setup and tear-down)", function() {
+describe("Servers test (with setup and tear-down)", function () {
   beforeEach(function () {
     // initialization logic
     serverNameInput.value = 'Alice';
@@ -10,8 +10,18 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(Object.keys(allServers).length).toEqual(1);
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
-
-  afterEach(function() {
-    // teardown logic
+  it('should add a new element to servertable on updateServerTable()', function(){
+    submitServerInfo(); //updateServerTable is called within
+    let serverTableBody = document.querySelector('#serverTable tbody tr'); //selects the new tr added by updateServerTable
+    expect(serverTableBody.childElementCount).toEqual(2);
+    expect(serverTableBody.children[0].innerHTML).toEqual('Alice');
+    expect(serverTableBody.children[1].innerHTML).toEqual('$0.00');
   });
+
+  afterEach(function () {
+    // teardown logic
+    allServers = {};
+    serverId = 0;
+    serverTbody.innerHTML = '';
+  })
 });
